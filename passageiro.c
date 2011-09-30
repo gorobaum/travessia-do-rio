e3b346e2c24f5f30d4b5ce1d50c24285e47e925c
Gorobaum <gorobaum@hotmail.com>
2011-09-30 18:02:24 -0300

    Vendo como memoria compartilhada funciona.

--- passageiro.c
+++ passageiro.c
@@ -10,31 +10,52 @@
 #define ESQUERDA 0
 #define DIREITA 1
 
+union semun {
+    int val; struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *__buf;
+};
 
-void embarca(int margem)
-{
+
+char* getMemo(int memokey, int size) {
+    int shmid;
+    shmid = shmget(memokey, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
+    return (char *)shmat(shmid, 0, 0);
+}
+
+
+void embarca(int margem) {
     /* Aqui o passageiro embarca na margem especificada se possivel
     ou espera o barco chegar a sua margem do rio */
 }
-void desembarca(int margem)
-{
+
+void desembarca(int margem) {
     /* Aqui o passageiro desembarca do barco vindo da margem especificada
     e realiza quaisquer outras tarefas para dar continuidade a viagem
     de outros passageiros */
 }
-void atravessa(int margem)
-{
+
+void atravessa(int margem) {
     /* O barco atravessa o rio a partir da margem especificada */
 }
-int main(int argc, char *argv[])
-{
-    int margem;
+
+int main(int argc, char *argv[]) {
+    int margem, memokey = 1;
+    char* saddr;
+    union semun arg;
+    struct sembuf wait={0, -1, 0};
+    struct sembuf signal={0, 1, 0};
+
+    saddr = getMemo(memokey, 10*sizeof(int) );
+    if ( saddr[0] == 0 ) {};
+
+
     if ( argc != 2 ) printf("Passar margem. 0 = Esquerda, 1 = Direita. \n");
     else {
         margem = atoi(argv[1]);
-        embarca(1);
-        atravessa(1);
-        desembarca(1);
+        embarca(margem);
+        atravessa(margem);
+        desembarca(margem);
     }
     /* imprime passageiro saiu do pier */
     exit(0);
