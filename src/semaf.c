dec5b4eb4881ddb244c2cc3f9e60fedf7f9597a0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2011-10-12 17:13:48 -0300

    Solução provisória para a inicialização dos semáforos.

--- src/semaf.c
+++ src/semaf.c
@@ -8,12 +8,18 @@
 
 #include "semaf.h"
 
+#define OP_NUM              (2)
+#define OP_WAIT             (-1)
+#define OP_SIGNAL           (1)
+
 /* Acho que vamos precisar de quatro semáforos. */
 #define SEM_NUM             (3)
 /* Semáforo MUTEX da memória compartilhada. */
 #define SHM_MUTEX           (0)
 /* Semáforos MUTEX para entrar no barco em cada margem do rio. */
 #define SHIP_MUTEX(margin)  ((margin%2)+1)
+
+#define SEMOP(op, sem) { sem, op, 0 }
 
 /* Semáforos. */
 static struct {
@@ -25,13 +31,18 @@ static int getSemKey() {
 }
 
 void semInit() {
-    union semun arg;
-    int semkey = getSemKey();
+    union semun     arg;
+    int             semkey = getSemKey();
+    unsigned short  initial_values[] = { 0, 0, 0 };
+    struct sembuf   signal_shm = SEMOP(OP_SIGNAL, SHM_MUTEX);
 
-    printf("SEM key: %x\n", semkey);
-    sem.id = semget( semkey, SEM_NUM, IPC_CREAT | 0666 );
-    arg.val = 1;
-    semctl(sem.id, 0, SETVAL, arg);
+    arg.array = initial_values;
+    printf("SEM key: 0x%x\n", semkey);
+    if ((sem.id = semget(semkey, SEM_NUM, IPC_CREAT | IPC_EXCL | 0666)) != -1) {
+        semctl(sem.id, 0, SETALL, arg);
+        semop(sem.id, &signal_shm, 1);
+    }
+    else sem.id = semget(semkey, SEM_NUM, 0666 );
 }
 
 void semCtl( int id, union semun arg ) {
